79501be3ee6c3b6c39a4eb58f7371d62897c759d
Klodian Imeri <klodianimeri@gmail.com>
2017-09-30 16:25:25 +0200

    remove old files

--- index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var horizontal_slider_module_1 = require("./lib/horizontal-slider.module");
-exports.HorizontalSliderModule = horizontal_slider_module_1.HorizontalSliderModule;
--- lib/hs-slide/hs-slide.component.d.ts
+++
@@ -1,7 +0,0 @@
-import { Renderer, ElementRef } from '@angular/core';
-export declare class HsSlideComponent {
-    private _renderer;
-    private _element;
-    constructor(_renderer: Renderer, _element: ElementRef);
-    SetWidth(width: number): void;
-}
